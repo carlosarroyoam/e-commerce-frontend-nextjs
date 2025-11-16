6c9872065e3817085975706140cfb72a391c5876
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2025-11-15 20:56:23 -0600

    run prettier --write .

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,9 +2,7 @@ const Footer = () => {
   return (
     <footer className="border-t py-4">
       <div className="container flex items-center justify-between">
-        <p className="text-sm">
-          &copy; e-commerce. All rights reserved.
-        </p>
+        <p className="text-sm">&copy; e-commerce. All rights reserved.</p>
         <span className="text-sm">v1.0.0-snapshot</span>
       </div>
     </footer>
